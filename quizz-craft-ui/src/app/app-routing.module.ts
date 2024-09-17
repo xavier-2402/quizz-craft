e4898f0a838ecb5fc2c242f1c005b13f38390b3c
Xavier Yanza <xavier.y.2013@gmail.com>
2024-09-17 00:21:07 -0500

    fix sidebar

--- quizz-craft-ui/src/app/app-routing.module.ts
+++ quizz-craft-ui/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NewChatComponent } from './components/new-chat/new-chat.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent,children:[
-    {path:'', component:NewChatComponent}
-  ]}
+  {
+    path:'',component:HomeComponent, children:[
+      {path:'new-chat',component:NewChatComponent},
+      {path:'chat',component:ChatComponent}
+    ]
+  },
 ];
 
 @NgModule({
